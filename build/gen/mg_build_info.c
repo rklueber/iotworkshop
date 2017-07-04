976556492a8a1d0ac1fd01c05e777c6618436279
unknown <rak@P3-05025.p3-group.com>
2017-07-04 17:37:10 +0200

    step 1

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20170701-160833/???";
-const char *mg_build_timestamp = "2017-07-01T16:08:33Z";
-const char *mg_build_version = "2017070116";
